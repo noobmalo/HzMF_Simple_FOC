078448d0b65b253ef620b5f3c9415e8d26bc5fc5
noobmalo <chrisfei0210@gmail.com>
2024-10-28 21:29:43 +0800

    Create cmake_clean.cmake

---
+++ Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/can.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/i2c.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/spi.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Startup/startup_stm32f103cbtx.s.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "HzMF_FOC_F103_MT6701.elf"
+  "HzMF_FOC_F103_MT6701.elf.manifest"
+  "HzMF_FOC_F103_MT6701.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
