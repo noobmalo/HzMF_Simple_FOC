aeb12ec717829cf48ed0c8ce3d986eb792297ec8
noobmalo <chrisfei0210@gmail.com>
2024-10-28 21:35:32 +0800

    Create DependInfo.cmake

---
+++ Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Startup/startup_stm32f103cbtx.s" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Startup/startup_stm32f103cbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Inc"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/can.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/can.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/gpio.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/gpio.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/i2c.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/i2c.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/main.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/main.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/spi.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/spi.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/stm32f1xx_hal_msp.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/stm32f1xx_it.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/syscalls.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/syscalls.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/sysmem.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/sysmem.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Src/system_stm32f1xx.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/cmake-build-debug/CMakeFiles/HzMF_FOC_F103_MT6701.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Core/Inc"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "C:/Users/chris/Desktop/GitHub/HzMF_Simple_FOC/Firmware/HzMF_FOC_F103_MT6701/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
